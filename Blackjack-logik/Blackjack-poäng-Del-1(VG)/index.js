8af9daaa126bce6928c01f932b455d18777ba284
tZandr <alex_tjernstrom@hotmail.com>
2025-10-28 15:45:22 +0100

    testing public repo

---
+++ Blackjack-logik/Blackjack-poäng-Del-1(VG)/index.js
@@ -0,0 +1,17 @@
+/*
+* Validator uses a deck of its own but I used mine to test.
+
+const deck = [];
+let suits = ["HEARTS", "SPADES", "DIAMONDS", "CLUBS"];
+
+for (const suit of suits) {
+  for (let value = 1; value <= 13; value++) {
+    deck.push({ suit, value });
+  }
+}
+
+console.log(deck); // Original deck
+
+--- */
+
+//public repo test
