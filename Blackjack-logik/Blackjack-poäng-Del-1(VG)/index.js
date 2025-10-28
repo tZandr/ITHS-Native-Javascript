ea6e21de30d43fe2818185d96e21dfca027777ea
tZandr <alex_tjernstrom@hotmail.com>
2025-10-28 19:04:05 +0100

    Validated and passed

--- Blackjack-logik/Blackjack-poäng-Del-1(VG)/index.js
+++ Blackjack-logik/Blackjack-poäng-Del-1(VG)/index.js
@@ -1,5 +1,4 @@
-/*
-* Validator uses a deck of its own but I used mine to test.
+// Validator uses a deck of its own but I used mine to test.
 
 const deck = [];
 let suits = ["HEARTS", "SPADES", "DIAMONDS", "CLUBS"];
@@ -10,8 +9,35 @@ for (const suit of suits) {
   }
 }
 
-console.log(deck); // Original deck
+console.log(deck);
 
---- */
+const score = function (deck) {
+  let totalScore = 0;
 
-//public repo test
+  for (const card of deck) {
+    if (card.value === 1) {
+      totalScore += 11;
+    } else if (card.value >= 11 && card.value <= 13) {
+      totalScore += 10;
+    } else {
+      totalScore += card.value;
+    }
+  }
+  return totalScore;
+};
+
+console.log(
+  score([
+    { suit: "HEARTS", value: 1 },
+    { suit: "HEARTS", value: 10 },
+  ])
+); // Returns 11
+console.log(score([{ suit: "SPADES", value: 2 }])); // Returns 2
+console.log(score([{ suit: "SPADES", value: 11 }])); // Returns 10
+console.log(
+  score([
+    { suit: "HEARTS", value: 2 },
+    { suit: "HEARTS", value: 3 },
+    { suit: "HEARTS", value: 4 },
+  ])
+); // Returns 9
