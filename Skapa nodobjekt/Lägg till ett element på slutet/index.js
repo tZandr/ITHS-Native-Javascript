44ff21bb35fbdd6f79f6650c04fcc9ea634e96ac
Alexander Tjernström <163892547+tZandr@users.noreply.github.com>
2025-11-07 15:01:33 +0100

    Validated

---
+++ Skapa nodobjekt/Lägg till ett element på slutet/index.js
@@ -0,0 +1,6 @@
+let listElement = document.createElement('li')
+let list = document.querySelector('ol')
+list.appendChild(listElement)
+listElement.textContent = 'Hi'
+
+// Add a new list item to the end of the ol
