2b0d83fe63d341421e5eff6f0f6ed618048bb06e
tZandr <alex_tjernstrom@hotmail.com>
2025-11-12 11:10:53 +0100

    Validated

--- Global eventListeners, and event-info/Tangentkod/index.js
+++ Global eventListeners, and event-info/Tangentkod/index.js
@@ -1,15 +1,5 @@
-let value = document.querySelector('#value')
-value.textContent = 1
-let plus = document.querySelector('#increase')
-let minus = document.querySelector('#decrease')
-
-function increase(){
-  value.textContent = Number(value.textContent) + 1
+function logKey(keyboardEvent){
+  console.log(keyboardEvent.key)
 }
 
-function decrease(){
-  value.textContent = Number(value.textContent) - 1
-}
-
-plus.addEventListener('click', increase)
-minus.addEventListener('click', decrease)
+addEventListener('keydown', logKey)
