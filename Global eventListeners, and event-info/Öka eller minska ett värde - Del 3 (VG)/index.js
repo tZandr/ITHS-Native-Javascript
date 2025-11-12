0e29e74c34d8644b7aebe81b1fc4a9d4bb4f1ed4
tZandr <alex_tjernstrom@hotmail.com>
2025-11-12 13:49:12 +0100

    Validated, module complete

--- Global eventListeners, and event-info/Öka eller minska ett värde - Del 3 (VG)/index.js
+++ Global eventListeners, and event-info/Öka eller minska ett värde - Del 3 (VG)/index.js
@@ -1,9 +1,38 @@
-let value = document.querySelector('#value')
-value.textContent = 1
-let element = document.querySelector('#increase')
+let value = document.querySelector("#value");
+value.textContent = 1;
+let plus = document.querySelector("#increase");
+let minus = document.querySelector("#decrease");
+minus.disabled = true;
 
-function increase(){
-  value.textContent = Number(value.textContent) + 1
+function increase() {
+  value.textContent = Number(value.textContent) + 1;
+  updateButton();
 }
 
-element.addEventListener('click', increase)
+function decrease() {
+  if (value.textContent > 1) {
+    value.textContent = Number(value.textContent) - 1;
+  }
+  updateButton();
+}
+
+function updateButton() {
+  if (Number(value.textContent) <= 1) {
+    minus.disabled = true;
+  } else {
+    minus.disabled = false;
+  }
+}
+
+plus.addEventListener("click", increase);
+minus.addEventListener("click", decrease);
+
+function logKey(keyboardEvent) {
+  if (keyboardEvent.key === "ArrowUp") {
+    increase();
+  } else if (keyboardEvent.key === "ArrowDown") {
+    decrease();
+  }
+}
+
+addEventListener("keydown", logKey);
